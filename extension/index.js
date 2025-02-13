7481755936b0b3ba66bce707ce2d4b988761f777
Adam Nowak <nowak.adam2222@gmail.com>
2025-02-13 22:12:12 +0100

    laid out code functions and fixed settings saving

--- extension/index.js
+++ extension/index.js
@@ -1,14 +1,29 @@
 const sidebar = document.querySelector(".p-show__sb");
 const paperGui = document.createElement("div");
 
-const settings = localStorage.getItem("psol-settings") || {
+let settings = localStorage.getItem("psol-settings") || {
     guiPosition: 4,
     balance: 1,
 };
 
 const updateSetting = (setting, value) => {
     settings[setting] = value;
-    localStorage.setItem("psol-settings", JSON.stringify(settings));
+    console.log(settings[setting]);
+    localStorage.setItem("psol-settings", settings);
+};
+
+const getCurrentMcap = () => {
+    const mcapElement = document.querySelector(".js-cable-val__mktcap");
+    const mcap = mcapElement.getAttribute("data-value");
+    return mcap;
+}
+
+const buy = (buyAmount) => {
+
+};
+
+const sell = (sellAmount) => {
+
 };
 
 const insertGUI = () => {
@@ -28,10 +43,10 @@ const insertGUI = () => {
                     <div class="setting">
                         <p>Interface position:</p>
                         <div class="setting-options">
-                            <button>
+                            <button class="move-gui-up">
                                 <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13 9H10V16H6V9L3 9V8L8 3L13 8V9Z" fill="#FFFFFF"/><path d="M14 2H2V0H14V2Z" fill="#FFFFFF"/></svg>
                             </button>
-                            <button>
+                            <button class="move-gui-down">
                                 <svg transform="rotate(180)" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13 9H10V16H6V9L3 9V8L8 3L13 8V9Z" fill="#FFFFFF"/><path d="M14 2H2V0H14V2Z" fill="#FFFFFF"/></svg>
                             </button>
                         </div>
@@ -40,7 +55,9 @@ const insertGUI = () => {
             </div>
         </div>
     `;
+
     sidebar.insertBefore(paperGui, sidebar.childNodes[settings.guiPosition]);
+
     const toggleSettings = document.querySelector(".toggle-settings");
     const settingsList = document.querySelector(".settings-list");
     settingsList.style.display = "none";
@@ -51,6 +68,19 @@ const insertGUI = () => {
             settingsList.style.display = "none";
         }
     });
+
+    const guiPosUpButton = document.querySelector(".move-gui-up");
+    const guiPosDownButton = document.querySelector(".move-gui-down");
+    guiPosUpButton.addEventListener("click", () => {
+        updateSetting("guiPosition", settings.guiPosition - 2);
+        insertGUI();
+    });
+    guiPosDownButton.addEventListener("click", () => {
+        updateSetting("guiPosition", settings.guiPosition + 2);
+        insertGUI();
+    });
+
+
     const balanceDisplay = document.querySelector(".balance-display");
     balanceDisplay.innerHTML = settings.balance;
     console.log("gui inserted");
